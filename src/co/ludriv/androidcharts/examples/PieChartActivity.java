b0452d7f5ded480cc6c23be5c18f810ef212b398
Ludovic <ludovic@ipstand.fr>
2014-07-05 14:26:17 +0200

    Pie chart example updated

--- src/co/ludriv/androidcharts/examples/PieChartActivity.java
+++ src/co/ludriv/androidcharts/examples/PieChartActivity.java
@@ -1,13 +1,17 @@
 package co.ludriv.androidcharts.examples;
 
 import co.ludriv.androidcharts.R;
+import co.ludriv.androidcharts.listeners.PieChartSegmentListener;
+import co.ludriv.androidcharts.models.PieChartSegment;
 import co.ludriv.androidcharts.views.PieChartView;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 
-public class PieChartActivity extends Activity
+public class PieChartActivity extends Activity implements PieChartSegmentListener
 {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -16,9 +20,24 @@ public class PieChartActivity extends Activity
 		
 		PieChartView pieChartView = (PieChartView) findViewById(R.id.example_piechartview);
 		
-		pieChartView.addValue(0.52, Color.GREEN, "China");
-		pieChartView.addValue(0.16, Color.BLUE, "France");
-		pieChartView.addValue(0.20, Color.RED, "Brazil");
-		pieChartView.addValue(0.12, Color.MAGENTA, "Colombia");
+		pieChartView.addSegment(0.52, Color.GREEN, "China");
+		pieChartView.addSegment(0.16, Color.BLUE, "France");
+		pieChartView.addSegment(0.20, Color.RED, "Brazil");
+		pieChartView.addSegment(0.12, Color.MAGENTA, "Colombia");
+		
+		pieChartView.setSegmentListener(this);
 	}
+
+	@Override
+	public void onSegmentSelected(PieChartView parent, PieChartSegment segment)
+	{
+		Log.d("Android-Charts", "selected segment: " + segment.getLabel());
+	}
+
+	@Override
+	public void onSegmentDeselected(PieChartView parent, PieChartSegment segment)
+	{
+		Log.d("Android-Charts", "deselected segment: " + segment.getLabel());
+	}
+	
 }
